c54e6742b6de9b32d9f2318df816ca447d6bc069
badbodfilfil <fildotov@gmail.com>
2017-12-19 05:51:12 +0300

    eee boy

--- aura/projectItem/projectItemController.js
+++ aura/projectItem/projectItemController.js
@@ -1,5 +1,7 @@
 ({
-    
+    doInit: function(component, event, helper) {
+        component.set('v.progress', component.get("v.project.Donations__c")/component.get("v.project.Budget__c")*100);
+    },
     openProjectInformation: function(component, event, helper) {
         console.log("in projectitem controller before fire evt");
         var evt = $A.get("e.force:navigateToComponent");
